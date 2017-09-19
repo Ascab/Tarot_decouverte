9d99614a90af8de661cd19055987f3340cdb27a5
Tineyo <alexis.barotin@outlook.fr>
2017-09-19 15:50:04 +0200

    tg

---
+++ headers/struct.h
@@ -0,0 +1,27 @@
+typedef enum val_classic{1,2,3,4,5,6,7,8,9,10,11,12,13,14} VALC;
+typedef enum val_atout{excuse,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21} VALA;
+typedef enum type  {ATOUT,CLASSIC} TYPE;
+typedef enum couleurC {trefle,coeur,pique,carreau} COULEURC;
+
+typedef struct
+{
+	VALC valC;
+	COULEURC couleur; 
+}CARTEC;
+
+typedef struct
+{
+	VALA valA; 
+}CARTEA;
+
+typedef struct
+{
+	TYPE typeC;
+	union
+	{
+		CARTEC carteC;
+		CARTEA carteA;
+	};
+}CARTE;
+
+
